32e84eaaec9a48f72250a715a32427b5e15e31fa
Gary Marigliano <gmarigliano93@gmail.com>
2016-11-21 13:12:26 +0100

    Add DummyDataset

---
+++ datasets/DummyDataset.py
@@ -0,0 +1,18 @@
+from datasets.Dataset import Dataset
+
+
+class DummyDataset(Dataset):
+    """
+    Just a dummy dataset for the tests
+    """
+    def __init__(self):
+        self._X = [
+            [60, 180],
+            [80, 180],
+            [40, 150],
+            [70, 170]
+        ]
+        self._y = ["M", "M", "M", "F"]
+
+    def _parse_labels(self, filenames):
+        pass
