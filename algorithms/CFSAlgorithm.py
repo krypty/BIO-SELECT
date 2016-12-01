a0b093305206f8f432c40d5c289095c21f5b4d2c
Gary Marigliano <gmarigliano93@gmail.com>
2016-12-01 12:03:30 +0100

    Add CFS and SVM forward algorithms

---
+++ algorithms/CFSAlgorithm.py
@@ -0,0 +1,28 @@
+from skfeature.function.statistical_based import CFS
+
+from algorithms.Algorithm import Algorithm, NotSupportedException
+
+
+class CFSAlgorithm(Algorithm):
+    """
+    This algorithm is determinist
+    """
+
+    def __init__(self, dataset, n=None):
+        super(CFSAlgorithm, self).__init__(dataset, n, "CFS")
+
+        if n is not None:
+            print("[warning] n argument will be ignored")
+
+        X = dataset.get_X()
+        y = dataset.get_y()
+        self._F = CFS.cfs(X, y)
+
+    def get_best_features(self):
+        return self._F
+
+    def get_best_features_by_rank(self):
+        raise NotSupportedException()
+
+    def _get_best_features_by_score_unnormed(self):
+        raise NotSupportedException()
---
+++ algorithms/SVMForwardAlgorithm.py
@@ -0,0 +1,25 @@
+from skfeature.function.wrapper import svm_forward
+
+from algorithms.Algorithm import Algorithm, NotSupportedException
+
+
+class SVMForwardAlgorithm(Algorithm):
+    """
+    This algorithm is NOT determinist
+    """
+    def __init__(self, dataset, n=None):
+        super(SVMForwardAlgorithm, self).__init__(dataset, n, "SVM Forward")
+
+        # execute this algorithm on all the dataset because internally it uses a KFold
+        X = dataset.get_X()
+        y = dataset.get_y()
+        self._F = svm_forward.svm_forward(X, y, n)
+
+    def get_best_features(self):
+        return self._F
+
+    def get_best_features_by_rank(self):
+        raise NotSupportedException()
+
+    def _get_best_features_by_score_unnormed(self):
+        raise NotSupportedException()
