6dcbce23bbdf006ab2dac084d8b0a929f4451cdc
Gary Marigliano <gmarigliano93@gmail.com>
2016-12-01 12:42:24 +0100

    Add MRMR algorithm

---
+++ algorithms/MRMRAlgorithm.py
@@ -0,0 +1,25 @@
+from skfeature.function.information_theoretical_based import MRMR
+
+from algorithms.Algorithm import Algorithm, NotSupportedException
+
+
+class MRMRAlgorithm(Algorithm):
+    """
+    This algorithm is determinist
+    """
+
+    def __init__(self, dataset, n=None):
+        super(MRMRAlgorithm, self).__init__(dataset, n, "MRMR")
+
+        X = dataset.get_X()
+        y = dataset.get_y()
+        self._idx = MRMR.mrmr(X, y, n_selected_features=n)
+
+    def get_best_features(self):
+        return self._idx
+
+    def get_best_features_by_rank(self):
+        return self._idx
+
+    def _get_best_features_by_score_unnormed(self):
+        raise NotSupportedException()
