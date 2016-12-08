e9d39bafa2c1c92f44fd7e615276769da10b3ac2
Gary Marigliano <gmarigliano93@gmail.com>
2016-12-08 09:52:38 +0100

    DatasetBalancer now only balances the train set if the dataset was split

--- datasets/DatasetBalancer.py
+++ datasets/DatasetBalancer.py
@@ -5,13 +5,22 @@ from datasets.Dataset import Dataset
 
 class DatasetBalancer:
     def __init__(self, dataset):
-        self._ds = dataset  # type: Dataset
+        self._ds = dataset  # type: Dataset or DatasetSplitter
         self._balancer = RandomOverSampler()
 
     def balance(self):
         """
-        Balance the classes of the dataset using a random oversampling
+        Balance the classes of the dataset using a random oversampling.
+        If the dataset has been split, then the balancer will only balance the train set
+        leaving the whole dataset get_X() and get_y() unchanged
+        Otherwise the balancer will balance the whole dataset.
         :return: the dataset oversampled
         """
-        self._ds._X, self._ds._y = self._balancer.fit_sample(self._ds.get_X(), self._ds.get_y())
+        try:
+            self._ds._X_train, self._ds._y_train = self._balancer.fit_sample(self._ds.get_X_train(),
+                                                                             self._ds.get_y_train())
+            print("train")
+        except AttributeError:
+            self._ds._X, self._ds._y = self._balancer.fit_sample(self._ds.get_X(), self._ds.get_y())
+            print("whole")
         return self._ds
--- tests/test_DatasetBalancer.py
+++ tests/test_DatasetBalancer.py
@@ -1,4 +1,5 @@
 from datasets.DatasetBalancer import DatasetBalancer
+from datasets.DatasetSplitter import DatasetSplitter
 from datasets.DummyDataset import DummyDataset
 
 import collections
@@ -23,3 +24,23 @@ class TestDatasetBalancer:
                    classes_cnt.values()), "Some classes are more or less represented than the others"
 
         assert len(ds.get_X()) == len(ds.get_y()), "X and y are not the same length !"
+
+    def test_oversampling_split(self):
+        ds = DummyDataset()
+
+        print(ds.get_X())
+        print(ds.get_y())
+
+        ds = DatasetSplitter(ds, test_size=0.5)
+        dsb = DatasetBalancer(ds)
+        ds = dsb.balance()
+
+        print(ds.get_X_train())
+        print(ds.get_y_train())
+
+        classes_cnt = collections.Counter(ds.get_y_train())
+
+        assert all(class_count == classes_cnt.values()[0] for class_count in
+                   classes_cnt.values()), "Some classes are more or less represented than the others"
+
+        assert len(ds.get_X_train()) == len(ds.get_y_train()), "X and y are not the same length !"
