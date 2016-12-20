9f9ebc12aa9858c785cc51d96e9cdb937d5e6bc2
Gary Marigliano <gmarigliano93@gmail.com>
2016-12-20 08:42:36 +0100

    Update Limma Docker scripts

--- docker-R/limma-golub.R
+++ docker-R/limma-golub.R
@@ -7,7 +7,8 @@ eset <- Golub_Merge
 model <- model.matrix(~pData(eset)$ALL.AML)
 fit <- eBayes(lmFit(log2(exprs(eset)), model), 0.01)
 tT <- topTable(fit, adjust="fdr", sort.by="B", number=1000)
-write.table(tT, file="dataset/limma-golub.csv", sep="\t")
+write.table(tT, file="/dataset/limma-golub.csv", sep="\t")
+#write.fit(fit, file="/dataset/limma-golub.csv", adjust="fdr", method="global", sep="\t")
 
 # fix header in csv file, add the ID column
-system("sed -i -e 's/\"logFC\"/\"ID\"\t\"logFC\"/' dataset/limma-golub.csv")
+system("sed -i -e 's/\"logFC\"/\"ID\"\t\"logFC\"/' /dataset/limma-golub.csv")
