5025a401a2122d8706c5f57061dc08dcc9f5297d
Gary Marigliano <gmarigliano93@gmail.com>
2017-01-25 13:48:08 +0100

    Updated documentation of UnionOfIntersectionsMerger

--- merge/techniques/UnionOfIntersectionsMerger.py
+++ merge/techniques/UnionOfIntersectionsMerger.py
@@ -7,6 +7,15 @@ from merge.techniques.SubsetMerger import SubsetMerger
 
 
 class UnionOfIntersectionsMerger(SubsetMerger):
+    """
+    Take the union of all the intersections of the lists taken two by two (sliding window).
+
+    Warning: this merging technique does not care about the similarity between the lists.
+    Although "a" and "c" have common features, there is no guarantee that this merging technique
+    will intersect "a" and "c" at all. For example, this can happens : "a" inter "c", "c" inter "b", "b" inter "d"
+    resulting a merged list with only the feature "1" (the only that is in all lists)
+    """
+
     def __init__(self, subsets):
         super(UnionOfIntersectionsMerger, self).__init__(subsets)
 
@@ -30,6 +39,8 @@ class UnionOfIntersectionsMerger(SubsetMerger):
 
         merged_subset = []
 
+        # sort the lists by decreasing length allow to always have the lists merged in the same order and it allows
+        # to do the intersections between lists with lists about the same size
         sort_by_len_features = sorted(self._subsets.values(), key=lambda x: len(x), reverse=True)
         lists_of_features = [([a[0] for a in f]) for f in sort_by_len_features]
 
@@ -41,7 +52,12 @@ class UnionOfIntersectionsMerger(SubsetMerger):
 
 
 if __name__ == '__main__':
-    s = [{1, 2, 4}, {1, 2, 3}, {10, 40, 60}]
+    s = {
+        "a": [(1, 0), (2, 0), (4, 0)],
+        "b": [(1, 0), (2, 0), (3, 0)],
+        "c": [(10, 0), (40, 0), (1, 0)],
+        "d": [(20, 0), (40, 0), (1, 0)]
+    }
 
     merged = UnionOfIntersectionsMerger(s).merge()
     print(merged)
