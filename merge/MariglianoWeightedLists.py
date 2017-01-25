a97b0ada2da78fbafa1eae7a709c2c29038fc653
Gary Marigliano <gmarigliano93@gmail.com>
2017-01-25 13:06:44 +0100

    Improve documentation for MariglianoWeightedLists

--- merge/MariglianoWeightedLists.py
+++ merge/MariglianoWeightedLists.py
@@ -1,5 +1,14 @@
-class MariglianoWeightLists:
+from __future__ import division
+import numpy as np
+
+
+class MariglianoWeightedLists:
     def __init__(self, Z, L):
+        """
+        This algorithm will use a linkage matrix Z to compute weights for the lists used by the matrix Z.
+        :param Z: the linkage matrix, Warning: 3x3 shaped not 3x4 like scipy returns. Drop the last column if needed.
+        :param L: the number of lists used in Z
+        """
         self._Z = Z
         self._L = L
 
@@ -8,15 +17,14 @@ class MariglianoWeightLists:
         W = np.zeros(self._L)
         self._update_weights(tree, z_weights, W, max_balance=1.0, parent_node_weight=z_weights[tree.id])
 
-        # reverse the weights since we want to promote the diversity
-        s = sum([1.0 / w for w in W])
-        print("W", W)
-        W = map(lambda w: (1.0 / w) / s, W)
-        print("W", W)
-        print("W sum", sum(W))
+        assert abs(sum(W) - 1.0) < 1e-3
         return W
 
     def _build_tree(self):
+        """
+        Build a tree from the linkage matrix Z.
+        :return: the tree
+        """
         roots = []
         z_weights = {}
 
@@ -41,9 +49,21 @@ class MariglianoWeightLists:
 
     @staticmethod
     def _find_max_node(roots):
+        """
+        Find the max root among all the roots (as "roots" contains multiple small trees of 3 nodes)
+        Example 3 -> (2,1) and 4 -> (3,0) will returns 4 -> (3,0)
+        :param roots:
+        :return: the max root which will be used as the top level node (root) for the tree.
+        """
         return max(roots, key=lambda x: x.id)
 
     def _replace_children(self, node, roots):
+        """
+        Replace the children of a node (which are integers at the beginning) with Node objects.
+        :param node: the node whose children will be replaced
+        :param roots: in order to retrieve the nodes objects from an integer
+        :return: the node with its children replaced by Node objects
+        """
         if isinstance(node, float) and node not in [r.id for r in roots]:
             return Node(node, 0, 0)
 
@@ -62,68 +82,88 @@ class MariglianoWeightLists:
 
     def _update_weights(self, node, z_weights, W, max_balance, parent_node_weight):
         """
-
-       :type node: Node
-       """
-        print(" ")
-        print(" ")
-        print("current node is", node.id)
-
+        Update the weights recursively by distributing the max_balance
+        :param node: the node to update
+        :param z_weights: a dictionary that contains the distances from the matrix
+        :param W: the weights
+        :param max_balance: the "amount"/ratio of weight we can use for this node (and its direct children)
+        :param parent_node_weight: the weight of the parent node
+        :return: W, the updated weights as a list
+        """
         # case 1: if both left child and right child are node
         if not node.lc.is_leaf() and not node.rc.is_leaf():
-            print("case 1")
             lc = node.lc
             rc = node.rc
-            print(lc.id, rc.id)
 
+            # instead of computing a ratio, we compute the difference between the nodes
             r = abs(z_weights[lc.id] - z_weights[rc.id])
-            rs = self._rescale(r, max_balance)
+            rs = self._rescale(r, max_balance, min_ratio=0.5)
 
             if z_weights[lc.id] > z_weights[rc.id]:
                 z_weights[lc.id] = rs
                 z_weights[rc.id] = max_balance - rs
             else:
-                z_weights[rc.id] = rs
                 z_weights[lc.id] = max_balance - rs
+                z_weights[rc.id] = rs
 
             self._update_weights(lc, z_weights, W, max_balance=z_weights[lc.id], parent_node_weight=z_weights[node.id])
             self._update_weights(rc, z_weights, W, max_balance=z_weights[rc.id], parent_node_weight=z_weights[node.id])
 
         # case 2: if left or right child is a node
         if (not node.lc.is_leaf() and node.rc.is_leaf()) or (node.lc.is_leaf() and not node.rc.is_leaf()):
-            print("case 2")
             lc = node.lc
             rc = node.rc
-            print(lc.id, rc.id)
 
+            # retrieve the child node and the leaf node
             child_node = lc if not lc.is_leaf() else rc
             leaf_node = lc if lc.is_leaf() else rc
 
+            # the ratio is computed between the parent node and the child node
             r = z_weights[child_node.id] / float(parent_node_weight)
-            rs = self._rescale(r, max_balance)
 
+            rs = self._rescale(r, max_balance, min_ratio=1.0 / 3.0)
+
+            # limit the weight to 2/3 if the ratio is too big. For example: 0.8/0.9 -> yield big ratio but lists are
+            # very different
+            rs = min(rs, 2.0 / 3.0)
             z_weights[child_node.id] = rs
             W[leaf_node.id] = max_balance - rs
 
+            # update the child node
             self._update_weights(child_node, z_weights, W, max_balance=z_weights[child_node.id],
                                  parent_node_weight=z_weights[node.id])
 
         # case 3: both left and right are leaves
         if node.lc.is_leaf() and node.rc.is_leaf():
-            print("case 3")
             lc = node.lc
             rc = node.rc
-            print(lc.id, rc.id)
+
+            # As they are no more nodes, the balance is split among the leaves
             W[lc.id] = 0.5 * max_balance
             W[rc.id] = 0.5 * max_balance
 
     @staticmethod
-    def _rescale(r, max_balance):
-        return r * (max_balance - 0.5 * max_balance) + 0.5 * max_balance
+    def _rescale(r, max_balance, min_ratio):
+        """
+        Re-scale the ratio of two nodes to be in [min_ratio*max_balance,max_balance].
+        :param r: ratio
+        :param max_balance: the maximum weight balance to use. This max_balance is equals to 1 for the root
+        and then it is split between the nodes as it goes down the tree.
+        :return: the re-scaled ratio
+        """
+        return r * (max_balance - min_ratio * max_balance) + min_ratio * max_balance
 
 
 class Node:
     def __init__(self, id, lc, rc):
+        """
+        Node is a element a of tree. It can be the root, a node or a leaf.
+        It contains an id a left child (lc) and a right child (rc).
+        If lc and rc are set to 0 then the node is a leaf. So lc and rc can both either a Node object or a int
+        :param id: the node id i.e. the id of the list
+        :param lc: the left child
+        :param rc: the right child
+        """
         self.id = int(id)
         self.lc = lc
         self.rc = rc
@@ -139,7 +179,6 @@ class Node:
 
 
 if __name__ == '__main__':
-    import numpy as np
     from matplotlib import pyplot as plt
 
 
@@ -164,29 +203,113 @@ if __name__ == '__main__':
         return ddata
 
 
-    # Demo matrix - all cases
-    L = 5
-    Z = [[0, 2, 0.1, 1],
-         [4, 3, 0.3, 1],
-         [1, 5, 0.4, 1],
-         [6, 7, 0.8, 1]]
+    examples = []
 
+    # # Demo matrix - all cases
+    # L = 5
+    # Z = [[0, 2, 0.1, 1],
+    #      [4, 3, 0.3, 1],
+    #      [1, 5, 0.4, 1],
+    #      [6, 7, 0.8, 1]]
+    # examples.append((L, Z, [1, 1, 1, 1, 1]))
+    #
+    # L = 3
+    # Z = [[2, 1, 0, 1],
+    #      [0, 3, 0.6, 1]]
+    # examples.append((L, Z, [1, 1, 1]))
+    #
+    # # right node near 0
+    # L = 3
+    # Z = [[0, 2, 0.1, 1],
+    #      [1, 3, 0.9, 1]]
+    # examples.append((L, Z, [0.1, 0.8, 0.1]))
+    #
+    # # left node near 0
+    # L = 3
+    # Z = [[0, 1, 0.1, 1],
+    #      [2, 3, 0.9, 1]]
+    # examples.append((L, Z, [0.1, 0.1, 0.8]))
+    #
+    # # 1/3 for all
+    # L = 3
+    # Z = [[0, 2, 0.8, 1],
+    #      [1, 3, 0.9, 1]]
+    # examples.append((L, Z, [0.33, 0.33, 0.33]))
+    #
     # # Neves example 1
     # L = 3
     # Z = [[0, 2, 0.25, 1],
     #      [1, 3, 0.8, 1]]
-
+    # examples.append((L, Z, [0.275, 0.45, 0.275]))
+    #
     # # Neves examples 2
     # L = 4
     # Z = [[1, 3, 0.25, 1],
     #      [0, 2, 0.8, 1],
     #      [4, 5, 1, 1]]
+    # examples.append((L, Z, [0.31, 0.19, 0.31, 0.19]))
+    #
+    # # 4 all equal
+    # L = 4
+    # Z = [[1, 3, 0.2, 1],
+    #      [0, 2, 0.2, 1],
+    #      [4, 5, 0.8, 1]]
+    # examples.append((L, Z, [0.25, 0.25, 0.25, 0.25]))
 
-    fancy_dendrogram(Z)
-    plt.show()
+    # A1
+    L = 3
+    Z = [[0, 2, 0.05, 1],
+         [1, 3, 0.8, 1]]
+    examples.append((L, Z, [0.15, 0.7, 0.15]))
 
-    Z = np.array(Z)
-    Z = Z[:, :-1]
+    # B1
+    L = 3
+    Z = [[0, 2, 0.25, 1],
+         [1, 3, 0.8, 1]]
+    examples.append((L, Z, [0.275, 0.45, 0.275]))
 
-    mwl = MariglianoWeightLists(Z, L)
-    mwl.compute_weights()
+    # C1
+    L = 3
+    Z = [[0, 2, 0.7, 1],
+         [1, 3, 0.8, 1]]
+    examples.append((L, Z, [0.3, 0.4, 0.3]))
+
+    # A2
+    L = 4
+    Z = [[1, 3, 0.25, 1],
+         [0, 2, 0.8, 1],
+         [4, 5, 1.0, 1]]
+    examples.append((L, Z, [0.31, 0.19, 0.31, 0.19]))
+
+    # B2
+    L = 4
+    Z = [[1, 3, 0.5, 1],
+         [0, 2, 0.7, 1],
+         [4, 5, 0.8, 1]]
+    examples.append((L, Z, [0.3, 0.2, 0.3, 0.2]))
+
+    # C2
+    L = 4
+    Z = [[1, 3, 0.2, 1],
+         [0, 2, 0.2, 1],
+         [4, 5, 0.8, 1]]
+    examples.append((L, Z, [0.25, 0.25, 0.25, 0.25]))
+
+    # for e in [examples[4]]:
+    i = 0
+    for e in examples:
+        L, Z, W = e
+
+        fancy_dendrogram(Z)
+
+        Z = np.array(Z)
+        Z = Z[:, :-1]  # get rid of the last column which is not used
+
+        mwl = MariglianoWeightedLists(Z, L)
+        mwl.compute_weights()
+
+        print("W expected", W)
+        print("")
+        print("")
+
+        plt.show()
